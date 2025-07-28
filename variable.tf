f64b4dc433090c8d4a6f0580f1bfb0de30b63ee7
vishnu4772 <gonuguntavishnu@gmail.com>
2025-07-28 23:18:34 +0530

    ec2

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
+}
